4108a650e5ee8b0fa137b337f064bbe1b4481579
Pablo Romo <33328647+pabsromo@users.noreply.github.com>
2021-01-05 20:17:40 -0600

    Finished Advanced React 2 - useEffect

--- react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,32 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
+// you can have as many useEffects as you want
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(()=> {
+    // The empty 'dependency list' as second argument when empty makes it so that
+    //   useEffect only runs on the initial render.
+    // When stuff inside 'dependency list' the useEffect runs only when the items
+    //   inside are changed.
+    // Used for stuff you want to do after each render.
+    console.log('call useEffect');
+    if(value >= 1) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value])
+
+  useEffect(() => {
+    console.log('hello world');
+  }, []);
+
+  console.log('render component');
+  return <>
+    <h1>{value}</h1>
+    <button className='btn' onClick={() => setValue(value + 1)}>click me</button>
+  </>;
 };
 
 export default UseEffectBasics;
